72ae49a396536c38799d62d4b14c2e5865395c85
surajmendhe5573 <surajmendhe470@gmail.com>
2025-03-28 19:33:05 +0530

    done

--- user-service/db/DB.js
+++ user-service/db/DB.js
@@ -2,10 +2,8 @@ const mongoose= require('mongoose');
 require('dotenv').config();
 
 const connectDB= ()=>{
-    mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(()=>{
+    mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
         console.log('Database is connected');  
     }).catch((err)=>{
         console.log('Database connection error: ', err);
